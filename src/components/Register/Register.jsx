7eba5e5a9f748482f7faa2e63bc95eb66093d5f3
mahinjaman07 <mahinjaman434@gmail.com>
2024-04-17 16:10:15 +0600

    complete the layout form in login and register page

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,9 +1,59 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 const Register = () => {
+    const handleRegister = e => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        const name = e.target.name.value;
+        console.log('firebase is coming');
+        console.log(email, password, name);
+    }
     return (
-        <div>
-            
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Register now!</h1>
+                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                </div>
+                <div className="card shrink-0 w-full max-w-lg shadow-2xl bg-base-100">
+                    <form className="card-body" onSubmit={handleRegister}>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Username:-</span>
+                            </label>
+                            <input type="text" name='name' placeholder="Name" className="input input-bordered" required />
+                        </div>
+
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" name='email' placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name='password' placeholder="password" className="input input-bordered" required />
+                            <div className='md:flex gap-2 justify-between'>
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+
+                                <label className="label">
+                                    <p className="label-text-alt">Already have an account? Please <Link className='underline' to={'/login'}>Login</Link></p>
+                                </label>
+                            </div>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Login</button>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
         </div>
     );
 };
