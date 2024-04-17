98399893e3a84d5d8fc7539a1b02d29db5d301c8
mahinjaman07 <mahinjaman434@gmail.com>
2024-04-18 00:39:52 +0600

    complete login and register page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,13 +1,71 @@
-import React from 'react';
+import { GithubAuthProvider, GoogleAuthProvider, sendPasswordResetEmail, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
+import React, { useRef, useState } from 'react';
+import auth from '../../firebase/firebase.config';
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+import { FaGoogle, FaFacebook, FaGithub } from "react-icons/fa";
 
 const Login = () => {
+    const [errorMessage, setErrorMessage ] = useState('');
+    const [successMessage, setSuccessMessage] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+
+    const emailRef = useRef(null);
     const handleLogin = e =>{
         e.preventDefault();
-        const email = e.target.email.value;
+        const email = emailRef.current.value;
         const password = e.target.password.value;
-        console.log('firebase is coming');
-        console.log(email, password);
+        if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)){
+            alert('Please enter a valid email');
+            return;
+        }
+        else if (!/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/.test(password)){
+            alert('Please enter a valid password');
+            return;
+        }
+        signInWithEmailAndPassword(auth, email, password)
+        .then(result =>{
+            const user = result.user;
+            console.log(user);
+            setSuccessMessage('You have successfully logged in');
+        })
+        .catch(err => {
+            console.log(err.message);
+            setErrorMessage(err.message);
+        })
     }
+
+    const handleForgetPassword = () =>{
+        console.log('Forget is coming');
+        sendPasswordResetEmail(auth, emailRef.current.value)
+        .then(result =>{
+            console.log(result);
+            alert('Please check your email')
+        })
+        .catch(err => {
+             console.log(err.message);
+             setErrorMessage(err.message);
+         })
+    }
+
+    const googleProvider = new GoogleAuthProvider();
+    const githubProvider = new GithubAuthProvider();
+
+    const handleOrLogin = provider =>{
+        setErrorMessage('')
+        setSuccessMessage('')
+        signInWithPopup(auth, provider)
+        .then(result =>{
+            const user = result.user;
+            console.log(user);
+            setSuccessMessage('You have successfully logged in');
+        })
+        .catch(err => {
+            console.log(err.message);
+            setErrorMessage(err.message);
+        })
+    }
+    
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row-reverse">
@@ -21,21 +79,45 @@ const Login = () => {
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="email" name='email' placeholder="email" className="input input-bordered" required />
+                            <input type="email" ref={emailRef} name='email' placeholder="email" className="input input-bordered" required />
                         </div>
-                        <div className="form-control">
+                        <div className="form-control relative">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" name='password' placeholder="password" className="input input-bordered" required />
+                            <input type={showPassword ? 'text' : 'password'} name='password' placeholder="password" className="input input-bordered" required />
+                            <span className='absolute right-5 top-[60%]' onClick={() => setShowPassword(!showPassword)}>{showPassword ? <FaRegEyeSlash></FaRegEyeSlash> : <FaRegEye></FaRegEye>}</span>
+                        </div>
+                        <div className='md:flex justify-between '>
                             <label className="label">
-                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                <button onClick={handleForgetPassword} className="label-text-alt link link-hover">Forgot password?</button>
+                            </label>
+
+                            <label className="label">
+                                <Link className="label-text-alt link link-hover" to={'/register'}>Register?</Link>
                             </label>
                         </div>
                         <div className="form-control mt-6">
                             <input className="btn btn-primary" type="submit" value="Login" />
                         </div>
+                        {
+                            errorMessage &&
+                            <p className="text-red-500">{errorMessage}</p>
+                        }
+                        {
+                            successMessage &&
+                            <p className="text-green-500">{successMessage}</p>
+                        }
                     </form>
+
+                    <div>
+                        <p>Or Login With </p>
+                        <div className='flex items-center justify-evenly w-5/12 mx-auto mt-4'>
+                            <button onClick={()=>handleOrLogin(googleProvider)} className='text-2xl'><FaGoogle></FaGoogle></button>
+                            <button onClick={()=>handleOrLogin(githubProvider)} className='text-2xl'><FaGithub></FaGithub></button>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
